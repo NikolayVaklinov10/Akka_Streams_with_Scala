df2092cd8628ad715d831a7e591eb3075f582487
Nick <nikolayvaklinov@yahoo.com>
2020-08-31 23:13:53 +0100

    the various type of sources for the akka streams

--- src/main/scala/part2_primer/FirstPrinciples.scala
+++ src/main/scala/part2_primer/FirstPrinciples.scala
@@ -4,6 +4,8 @@ import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.{Flow, Sink, Source}
 
+import scala.concurrent.Future
+
 object FirstPrinciples extends App {
 
   implicit val system = ActorSystem("FirstPrinciples")
@@ -38,4 +40,15 @@ object FirstPrinciples extends App {
   // nulls are not allow to emit elements
   val illegalSource = Source.single[String](null)
   illegalSource.to(Sink.foreach(println)).run()
+  // in order the null limitations to be overcome Options can be used
+
+  // different kind of sources
+  val finiteSource = Source.single(1)
+  val anotherFiniteSource = Source(List(1, 2, 3))
+  val emptySource = Source.empty[Int]
+  val infiniteSource = Source(Stream.from(1)) // do not confuse akka stream with a "collection" Stream
+
+  import scala.concurrent.ExecutionContext.Implicits.global
+  val futureSource = Source.fromFuture(Future(42))
+
 }
