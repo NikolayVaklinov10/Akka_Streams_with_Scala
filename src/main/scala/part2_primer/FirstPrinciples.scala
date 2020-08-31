27465d7feb097e21d023584c5450f3f6e618e1be
Nick <nikolayvaklinov@yahoo.com>
2020-08-31 22:32:50 +0100

    the first akka stream

--- src/main/scala/part2_primer/FirstPrinciples.scala
+++ src/main/scala/part2_primer/FirstPrinciples.scala
@@ -1,5 +1,26 @@
 package part2_primer
 
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.{Sink, Source}
+
 object FirstPrinciples extends App {
 
+  implicit val system = ActorSystem("FirstPrinciples")
+  implicit val materializer = ActorMaterializer()
+
+  // every akka stream starts with a source
+  // the source
+  val source = Source(1 to 10)
+  // every akka stream ends with a sink
+  // the sink
+  val sink = Sink.foreach[Int](println)
+
+  // in order to create an akka stream the source and the sink have to be connected!!!
+  // the so called graph
+  // the graph
+  val graph = source.to(sink)
+
+  // to start the stream the method run() has to called on the graph
+  graph.run()
 }
