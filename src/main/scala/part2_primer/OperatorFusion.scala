95d31aa30630ef87f4370047cd4dbcf78a22f1d3
Nick <nikolayvaklinov@yahoo.com>
2020-09-01 15:44:32 +0100

    a valid runnable graph

--- src/main/scala/part2_primer/OperatorFusion.scala
+++ src/main/scala/part2_primer/OperatorFusion.scala
@@ -2,10 +2,19 @@ package part2_primer
 
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.{Flow, Sink, Source}
 
 object OperatorFusion extends App {
 
   implicit val system = ActorSystem("OperatorFusion")
   implicit val materializer = ActorMaterializer()
+
+  val simpleSource = Source(1 to 1000) // 1. the source
+  val simpleFlow = Flow[Int].map(_ + 1) // 2. the flow #1
+  val simpleFlow2 = Flow[Int].map(_ * 10) // 2. the flow #2
+  val simpleSink = Sink.foreach[Int](println) // 3. the sink
+
+  // connecting all the components
+  simpleSource.via(simpleFlow).via(simpleFlow2).to(simpleSink).run() // a valid Akka Stream runable graph
 
 }
