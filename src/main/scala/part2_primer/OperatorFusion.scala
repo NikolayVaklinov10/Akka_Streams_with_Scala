e980d087bcdab2952e5b75410402009b97aa4c1d
Nick <nikolayvaklinov@yahoo.com>
2020-09-01 15:54:45 +0100

    exposing the problem

--- src/main/scala/part2_primer/OperatorFusion.scala
+++ src/main/scala/part2_primer/OperatorFusion.scala
@@ -1,6 +1,6 @@
 package part2_primer
 
-import akka.actor.ActorSystem
+import akka.actor.{Actor, ActorSystem, Props}
 import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.{Flow, Sink, Source}
 
@@ -19,6 +19,33 @@ object OperatorFusion extends App {
 
   // Attention this run on the same Actor a.k.a. operator/component FUSION
 
-  
+  // "equivalent" behavior
+  class SimpleActor extends Actor {
+    override def receive: Receive = {
+      case x: Int =>
+        // flow operations
+        val x2 = x + 1
+        val y = x2 * 10
+        // sink operation
+        println(y)
+    }
+  }
+  val simpleActor = system.actorOf(Props[SimpleActor])
+  //  (1 to 1000).foreach(simpleActor ! _)
+
+  // complex flows:
+  val complexFlow = Flow[Int].map { x =>
+    // simulating a long computation
+    Thread.sleep(1000)
+    x + 1
+  }
+  val complexFlow2 = Flow[Int].map { x =>
+    // simulating a long computation
+    Thread.sleep(1000)
+    x * 10
+  }
+
+  //  simpleSource.via(complexFlow).via(complexFlow2).to(simpleSink).run()
+
 
 }
