21258ad45a8d49ec2ea7450598d2bbb9a7222477
Nick <nikolayvaklinov@yahoo.com>
2020-09-01 10:58:09 +0100

    defining materializer and system for the code

--- src/main/scala/part2_primer/MaterializingStreams.scala
+++ src/main/scala/part2_primer/MaterializingStreams.scala
@@ -1,6 +1,12 @@
 package part2_primer
 
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+
 object MaterializingStreams extends App {
+
+  implicit val system = ActorSystem("MaterializingStreams")
+  implicit val materializer = ActorMaterializer
   
 
 }
