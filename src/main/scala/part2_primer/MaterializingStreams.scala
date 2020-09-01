6964d6c9533b03eda4de14312cd9b1b97b5d290e
Nick <nikolayvaklinov@yahoo.com>
2020-09-01 11:08:20 +0100

    source plus sink using runWith in order to create future

--- src/main/scala/part2_primer/MaterializingStreams.scala
+++ src/main/scala/part2_primer/MaterializingStreams.scala
@@ -2,11 +2,18 @@ package part2_primer
 
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.{Sink, Source}
 
 object MaterializingStreams extends App {
 
   implicit val system = ActorSystem("MaterializingStreams")
-  implicit val materializer = ActorMaterializer
-  
+  implicit val materializer = ActorMaterializer()
+
+  val simpleGraph = Source(1 to 10).to(Sink.foreach(println))
+  val simpleMaterializedValue = simpleGraph.run()
+
+  val source = Source(1 to 10)
+  val sink = Sink.reduce[Int]((a, b) => a + b)
+  val sumFuture = source.runWith(sink)
 
 }
