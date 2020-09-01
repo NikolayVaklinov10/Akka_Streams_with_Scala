d79ae4b8b85ec1c80b07a86d81f0971cac596c5b
Nick <nikolayvaklinov@yahoo.com>
2020-09-01 15:37:33 +0100

    initializing the actor system and actor materializer in order the stream code to compile

--- src/main/scala/part2_primer/OperatorFusion.scala
+++ src/main/scala/part2_primer/OperatorFusion.scala
@@ -1,5 +1,11 @@
 package part2_primer
 
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+
 object OperatorFusion extends App {
+
+  implicit val system = ActorSystem("OperatorFusion")
+  implicit val materializer = ActorMaterializer()
 
 }
