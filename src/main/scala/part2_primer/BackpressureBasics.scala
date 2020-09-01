11e23b67ddb5ea1d79c70fbb09ffd71e5a583758
Nick <nikolayvaklinov@yahoo.com>
2020-09-01 16:48:40 +0100

    back pressure in action the async stores elements in its buffer due to the slow down signaled by the client 'sink'

--- src/main/scala/part2_primer/BackpressureBasics.scala
+++ src/main/scala/part2_primer/BackpressureBasics.scala
@@ -2,7 +2,7 @@ package part2_primer
 
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
-import akka.stream.scaladsl.{Sink, Source}
+import akka.stream.scaladsl.{Flow, Sink, Source}
 
 object BackpressureBasics extends App {
 
@@ -16,7 +16,20 @@ object BackpressureBasics extends App {
     println(s"Sink: $x")
   }
 
-  fastSource.to(slowSink).run() // fusing?!
+//  fastSource.to(slowSink).run() // fusing?!
   // not back pressure
+
+//  fastSource.async.to(slowSink).run()
+  // back pressure
+
+  val simpleFlow = Flow[Int].map { x =>
+    println(s"Incoming: $x")
+    x + 1
+  }
+
+  fastSource.async
+    .via(simpleFlow).async
+    .to(slowSink)
+    .run()
 
 }
