1d0e5465b7e2018917cabd234a05ea37d09999b0
Nick <nikolayvaklinov@yahoo.com>
2020-09-01 13:21:02 +0100

    the future in akka streams in practice

--- src/main/scala/part2_primer/MaterializingStreams.scala
+++ src/main/scala/part2_primer/MaterializingStreams.scala
@@ -2,7 +2,7 @@ package part2_primer
 
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
-import akka.stream.scaladsl.{Sink, Source}
+import akka.stream.scaladsl.{Flow, Keep, Sink, Source}
 
 import scala.util.{Failure, Success}
 
@@ -17,10 +17,20 @@ object MaterializingStreams extends App {
 
   val source = Source(1 to 10)
   val sink = Sink.reduce[Int]((a, b) => a + b)
-  val sumFuture = source.runWith(sink)
-  sumFuture.onComplete {
-    case Success(value) => println(s"The sum of all elements is : $value")
-    case Failure(ex) => println(s"The sum of the elements could not be computed: $ex")
+//  val sumFuture = source.runWith(sink)
+//  sumFuture.onComplete {
+//    case Success(value) => println(s"The sum of all elements is : $value")
+//    case Failure(ex) => println(s"The sum of the elements could not be computed: $ex")
+//  }
+
+  // choosing materialized values
+  val simpleSource = Source(1 to 10)
+  val simpleFlow = Flow[Int].map(x => x + 1)
+  val simpleSink = Sink.foreach[Int](println)
+  val graph = simpleSource.viaMat(simpleFlow)(Keep.right).toMat(simpleSink)(Keep.right)
+  graph.run().onComplete {
+    case Success(_) => println("Stream processing finished.")
+    case Failure(exception) => println(s"Stream processing failed with : $exception")
   }
 
 }
