7b426ea90a3b5bdd6c049cf7a348f3b1b0c07ddc
Nick <nikolayvaklinov@yahoo.com>
2020-09-01 16:31:07 +0100

    initializing the actor system and materializer for the code

--- src/main/scala/part2_primer/BackpressureBasics.scala
+++ src/main/scala/part2_primer/BackpressureBasics.scala
@@ -1,5 +1,11 @@
 package part2_primer
 
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+
 object BackpressureBasics extends App {
+
+  implicit val system = ActorSystem("BackPressureBasics")
+  implicit val materializer = ActorMaterializer()
 
 }
