218d580cea4a3a2f5f9db9e0938b8b5d09ff57d4
Nick <nikolayvaklinov@yahoo.com>
2020-09-03 17:05:54 +0100

    the implicits needed for the code

--- src/main/scala/part3_graphs/GraphBasics.scala
+++ src/main/scala/part3_graphs/GraphBasics.scala
@@ -1,6 +1,13 @@
 package part3_graphs
 
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+
 object GraphBasics extends App {
-  
+
+  // the implicits for th code
+  implicit val system = ActorSystem("GraphBasics")
+  implicit val materializer = ActorMaterializer()
+
 
 }
