573207d39fa80d4a7b6b6783511889ad5d2eb6b7
Nick <nikolayvaklinov@yahoo.com>
2020-09-05 13:07:41 +0100

    requirements for the graph

--- src/main/scala/part3_graphs/OpenGraphs.scala
+++ src/main/scala/part3_graphs/OpenGraphs.scala
@@ -8,4 +8,10 @@ object OpenGraphs extends App {
   implicit val system = ActorSystem("OpenGraphs")
   implicit val materializer = ActorMaterializer()
 
+  /*
+    A composite source that concatenates 2 sources
+    - emits ALL the elements from the first source
+    - then ALL the elements from the second one
+   */
+
 }
