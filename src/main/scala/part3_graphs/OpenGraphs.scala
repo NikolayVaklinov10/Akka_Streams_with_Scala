b8d3eabad33897bbe88776796f848e203999959b
Nick <nikolayvaklinov@yahoo.com>
2020-09-05 13:04:58 +0100

    the regular actor materializer and actor system code

--- src/main/scala/part3_graphs/OpenGraphs.scala
+++ src/main/scala/part3_graphs/OpenGraphs.scala
@@ -1,5 +1,11 @@
 package part3_graphs
 
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+
 object OpenGraphs extends App {
+
+  implicit val system = ActorSystem("OpenGraphs")
+  implicit val materializer = ActorMaterializer()
 
 }
