5e629d6907593b9044f7daf8d4d40f0edeb2a5f2
Nick <nikolayvaklinov@yahoo.com>
2020-09-05 13:18:55 +0100

    first open source graph

--- src/main/scala/part3_graphs/OpenGraphs.scala
+++ src/main/scala/part3_graphs/OpenGraphs.scala
@@ -1,7 +1,8 @@
 package part3_graphs
 
 import akka.actor.ActorSystem
-import akka.stream.ActorMaterializer
+import akka.stream.{ActorMaterializer, SourceShape}
+import akka.stream.scaladsl.{Concat, GraphDSL, Source}
 
 object OpenGraphs extends App {
 
@@ -13,5 +14,25 @@ object OpenGraphs extends App {
     - emits ALL the elements from the first source
     - then ALL the elements from the second one
    */
+
+  val firstSource = Source(1 to 10)
+  val secondSource = Source(42 to 1000)
+
+  // step 1
+  val sourceGraph = Source.fromGraph(
+    GraphDSL.create() { implicit builder =>
+      import GraphDSL.Implicits._
+
+      // step 2: declaring components
+      val concat = builder.add(Concat[Int](2))
+
+      // step 3: tying them together
+      firstSource ~> concat
+      secondSource ~> concat
+
+      // step 4
+      SourceShape(concat.out)
+    }
+  )
 
 }
