faed4cadb22b8afffd0d929f74875d840dfcc79d
Nick <nikolayvaklinov@yahoo.com>
2020-09-06 13:19:37 +0100

    the standard implicit for the akka streams code

--- src/main/scala/part3_graphs/MoreOpenGraphs.scala
+++ src/main/scala/part3_graphs/MoreOpenGraphs.scala
@@ -1,5 +1,11 @@
 package part3_graphs
 
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+
 object MoreOpenGraphs extends App {
+
+  implicit val system = ActorSystem("MoreOpenGraphs")
+  implicit val materializer = ActorMaterializer()
 
 }
