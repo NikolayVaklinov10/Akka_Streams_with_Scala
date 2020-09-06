85ce4b3e691866062f5f6b08ea4748d2c63daad2
Nick <nikolayvaklinov@yahoo.com>
2020-09-07 00:09:57 +0100

    the final exercise

--- src/main/scala/part3_graphs/GraphMaterializedValues.scala
+++ src/main/scala/part3_graphs/GraphMaterializedValues.scala
@@ -1,9 +1,10 @@
 package part3_graphs
 
 import akka.actor.ActorSystem
-import akka.stream.{ActorMaterializer, SinkShape}
+import akka.stream.{ActorMaterializer, FlowShape, SinkShape}
 import akka.stream.scaladsl.{Broadcast, Flow, GraphDSL, Keep, Sink, Source}
 
+import scala.concurrent.Future
 import scala.util.{Failure, Success}
 
 object GraphMaterializedValues extends App {
@@ -23,7 +24,7 @@ object GraphMaterializedValues extends App {
 
   // step 1
   val complexWordSink = Sink.fromGraph(
-    GraphDSL.create(counter) { implicit builder => counterShape =>
+    GraphDSL.create(printer, counter)((printerMatValue, counterMatValue) => counterMatValue) { implicit builder => (printerShape,counterShape) =>
       import GraphDSL.Implicits._
 
       // step 2 - SHAPES
@@ -32,7 +33,7 @@ object GraphMaterializedValues extends App {
       val shortStringFilter = builder.add(Flow[String].filter(_.length < 5))
 
       // step 3 - connections
-      broadcast ~> lowercaseFilter ~> printer
+      broadcast ~> lowercaseFilter ~> printerShape
       broadcast ~> shortStringFilter ~> counterShape
 
       // step 4 - the shape
@@ -44,6 +45,36 @@ object GraphMaterializedValues extends App {
   shortStringsCountFuture.onComplete {
     case Success(count) => println(s"The total number of short strings is: $count")
     case Failure(exception) => println(s"The count of short strings failed: $exception")
+  }
+
+  /**
+   * Exercise
+   */
+
+  def enhanceFlow[A, B](flow: Flow[A,B, _]): Flow[A,B, Future[Int]] = {
+    val counterSink = Sink.fold[Int, B](0)((count, _) => count + 1)
+
+    Flow.fromGraph(
+      GraphDSL.create(counterSink) { implicit builder => counterSinkShape =>
+        import GraphDSL.Implicits._
+
+        val broadcast = builder.add(Broadcast[B](2))
+        val originalFlowShape = builder.add(flow)
+
+        originalFlowShape ~> broadcast ~> counterSinkShape
+
+        FlowShape(originalFlowShape.in, broadcast.out(1))
+      }
+    )
+  }
+  val simpleSource = Source(1 to 42)
+  val simpleFlow = Flow[Int].map(x => x)
+  val simpleSink = Sink.ignore
+
+  val enhancedFlowCountFuture = simpleSource.viaMat(enhanceFlow(simpleFlow))(Keep.right).toMat(simpleSink)(Keep.left).run()
+  enhancedFlowCountFuture.onComplete {
+    case Success(count) => println(s"$count elements went through the enhanced flow")
+    case _ => println("Something failed")
   }
 
 }
