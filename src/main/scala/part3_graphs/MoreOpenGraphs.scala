f8350dd7972388c99de62944b06d8cbdc1c49a3b
Nick <nikolayvaklinov@yahoo.com>
2020-09-06 14:00:58 +0100

    fixing the error on row 31

--- src/main/scala/part3_graphs/MoreOpenGraphs.scala
+++ src/main/scala/part3_graphs/MoreOpenGraphs.scala
@@ -1,11 +1,58 @@
 package part3_graphs
 
 import akka.actor.ActorSystem
-import akka.stream.ActorMaterializer
+import akka.stream.{ActorMaterializer, ClosedShape, UniformFanInShape}
+import akka.stream.scaladsl.{GraphDSL, RunnableGraph, Sink, Source, ZipWith}
 
 object MoreOpenGraphs extends App {
 
   implicit val system = ActorSystem("MoreOpenGraphs")
   implicit val materializer = ActorMaterializer()
 
+  /*
+    Example: Max3 operator
+    - 3 inputs of type int
+    - the maximum of the 3
+   */
+
+  // step 1
+  val max3StaticGraph = GraphDSL.create() { implicit builder =>
+    import GraphDSL.Implicits._
+
+    // step 2 - define aux SHAPE
+    val max1 = builder.add(ZipWith[Int, Int, Int]((a, b) => Math.max(a, b)))
+    val max2 = builder.add(ZipWith[Int, Int, Int]((a, b) => Math.max(a, b)))
+
+    // step 3
+    max1.out ~> max2.in0
+
+    // step 4
+
+    UniformFanInShape(max2.out, max1.in0, max1.in1, max2.in1)
+  }
+  val source1 = Source(1 to 10)
+  val source2 = Source(1 to 10).map(_ => 5)
+  val source3 = Source((1 to 10).reverse)
+
+  val maxSink = Sink.foreach[Int](x => println(s"Max is: $x"))
+
+  // step 1
+  val max3RunnableGraph = RunnableGraph.fromGraph(
+    GraphDSL.create() { implicit builder =>
+      import GraphDSL.Implicits._
+
+      // step 2 - declare SHAPES
+      val max3Shape = builder.add(max3StaticGraph)
+
+      // step 3 - tie
+      source1 ~> max3Shape.in(0)
+      source2 ~> max3Shape.in(1)
+      source3 ~> max3Shape.in(2)
+      max3Shape.out ~> maxSink
+
+      // step 4
+      ClosedShape
+    }
+  )
+  max3RunnableGraph.run()
 }
