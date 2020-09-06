ffa9643a2b646b2fd5ea71977db46cacf272381a
Nick <nikolayvaklinov@yahoo.com>
2020-09-07 00:17:34 +0100

    the implicits needed for the code

--- src/main/scala/part3_graphs/BidirectionalFlows.scala
+++ src/main/scala/part3_graphs/BidirectionalFlows.scala
@@ -1,5 +1,11 @@
 package part3_graphs
 
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+
 object BidirectionalFlows extends App {
 
+  implicit val system = ActorSystem("BidirectionalFlows")
+  implicit val materializer = ActorMaterializer()
+  
 }
