14d043469ee769b340c2c343c00ccb75634e5ea7
Nick <nikolayvaklinov@yahoo.com>
2020-09-07 11:55:31 +0100

    the opposite function to decrypt a string

--- src/main/scala/part3_graphs/BidirectionalFlows.scala
+++ src/main/scala/part3_graphs/BidirectionalFlows.scala
@@ -12,7 +12,9 @@ object BidirectionalFlows extends App {
     Example: cryptography
    */
   def encrypt(n: Int)(string: String) = string.map(c => (c + n).toChar)
+  def decrypt(n: Int)(string: String) = string.map(c => (c - n).toChar)
 
   println(encrypt(3)("Akka"))
+  println(decrypt(3)("Dnnd"))
 
 }
