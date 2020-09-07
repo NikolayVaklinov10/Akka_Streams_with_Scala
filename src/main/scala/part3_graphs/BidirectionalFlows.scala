b600ea2d71509f01c9abb0e8be30e27b9181fb41
Nick <nikolayvaklinov@yahoo.com>
2020-09-07 12:43:55 +0100

    bidirectional shape written in two ways

--- src/main/scala/part3_graphs/BidirectionalFlows.scala
+++ src/main/scala/part3_graphs/BidirectionalFlows.scala
@@ -1,7 +1,8 @@
 package part3_graphs
 
 import akka.actor.ActorSystem
-import akka.stream.ActorMaterializer
+import akka.stream.{ActorMaterializer, BidiShape}
+import akka.stream.scaladsl.{Flow, GraphDSL}
 
 object BidirectionalFlows extends App {
 
@@ -17,4 +18,16 @@ object BidirectionalFlows extends App {
   println(encrypt(3)("Akka"))
   println(decrypt(3)("Dnnd"))
 
+  // bidiFlow or Bidirectional Flow
+  val bidiCryptoStaticGraph = GraphDSL.create() { implicit builder =>
+    import GraphDSL.Implicits._
+
+    val encryptionFlowShape = builder.add(Flow[String].map(encrypt(3)))
+    val decryptionFlowShape = builder.add(Flow[String].map(decrypt(3)))
+
+    BidiShape(encryptionFlowShape.in, encryptionFlowShape.out, decryptionFlowShape.in, decryptionFlowShape.out)
+    // Or the other way to write this code
+    BidiShape.fromFlows(encryptionFlowShape, decryptionFlowShape)
+
+  }
 }
