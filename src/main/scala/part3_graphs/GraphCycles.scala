6eb020ebf8e7951e486de1f9348b02884aa5f3ef
Nick <nikolayvaklinov@yahoo.com>
2020-09-07 15:58:40 +0100

    the enhanced version of the code with the MergedPreferred method

--- src/main/scala/part3_graphs/GraphCycles.scala
+++ src/main/scala/part3_graphs/GraphCycles.scala
@@ -2,7 +2,7 @@ package part3_graphs
 
 import akka.actor.ActorSystem
 import akka.stream.{ActorMaterializer, ClosedShape}
-import akka.stream.scaladsl.{Flow, GraphDSL, Merge, RunnableGraph, Source}
+import akka.stream.scaladsl.{Flow, GraphDSL, Merge, MergePreferred, RunnableGraph, Source}
 
 object GraphCycles extends App {
 
@@ -25,6 +25,27 @@ object GraphCycles extends App {
 
     ClosedShape
   }
-  RunnableGraph.fromGraph(accelerator).run()
+//  RunnableGraph.fromGraph(accelerator).run()
   // the code above causing graph cycle deadlock!
+
+  /*
+    Solution 1: MergePreferred
+   */
+  val actualAccelerator = GraphDSL.create() { implicit builder =>
+    import GraphDSL.Implicits._
+
+    val sourceShape = builder.add(Source(1 to 100))
+    val mergeShape = builder.add(MergePreferred[Int](1))
+    val incrementerShape = builder.add(Flow[Int].map { x =>
+      println(s"Accelerating $x")
+      x + 1
+    })
+
+    sourceShape ~> mergeShape ~> incrementerShape
+    mergeShape.preferred <~ incrementerShape
+
+    ClosedShape
+  }
+  RunnableGraph.fromGraph(actualAccelerator).run()
+
 }
