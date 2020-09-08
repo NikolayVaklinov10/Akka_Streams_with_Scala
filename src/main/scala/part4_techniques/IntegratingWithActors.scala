f7625fe5ab900019ff9fea480474ade6257ac4b1
Nick <nikolayvaklinov@yahoo.com>
2020-09-08 23:24:44 +0100

    the implicits

--- src/main/scala/part4_techniques/IntegratingWithActors.scala
+++ src/main/scala/part4_techniques/IntegratingWithActors.scala
@@ -1,5 +1,11 @@
 package part4_techniques
 
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+
 object IntegratingWithActors extends App {
+
+  implicit val system = ActorSystem("IntegratingWithActors")
+  implicit val materializer = ActorMaterializer()
 
 }
