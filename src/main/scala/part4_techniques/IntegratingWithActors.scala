c80492e81b533c9bcf2c0edee8c5607fa33f737c
Nick <nikolayvaklinov@yahoo.com>
2020-09-08 23:31:06 +0100

    one simple actor

--- src/main/scala/part4_techniques/IntegratingWithActors.scala
+++ src/main/scala/part4_techniques/IntegratingWithActors.scala
@@ -1,6 +1,6 @@
 package part4_techniques
 
-import akka.actor.ActorSystem
+import akka.actor.{Actor, ActorLogging, ActorSystem}
 import akka.stream.ActorMaterializer
 
 object IntegratingWithActors extends App {
@@ -8,4 +8,16 @@ object IntegratingWithActors extends App {
   implicit val system = ActorSystem("IntegratingWithActors")
   implicit val materializer = ActorMaterializer()
 
+  class SimpleActor extends Actor with ActorLogging {
+
+    override def receive: Receive = {
+      case s: String =>
+        log.info(s"Just received a string: $s")
+        sender() ! s"$s$s"
+      case n: Int =>
+        log.info(s"Just received a number: $n")
+        sender() ! (2 * n)
+      case _ =>
+    }
+  }
 }
