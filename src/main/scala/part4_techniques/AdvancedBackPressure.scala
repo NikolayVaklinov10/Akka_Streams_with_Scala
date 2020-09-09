37fb7255059489a03c24c297c1a7a069d9d0bc07
Nick <nikolayvaklinov@yahoo.com>
2020-09-09 16:18:18 +0100

    in this way the backpressure can be controlled

--- src/main/scala/part4_techniques/AdvancedBackPressure.scala
+++ src/main/scala/part4_techniques/AdvancedBackPressure.scala
@@ -1,11 +1,15 @@
 package part4_techniques
 
 import akka.actor.ActorSystem
-import akka.stream.ActorMaterializer
+import akka.stream.{ActorMaterializer, OverflowStrategy}
+import akka.stream.scaladsl.Flow
 
 object AdvancedBackPressure extends App {
 
   implicit val system = ActorSystem("AdvancedBackPressure")
   implicit val materializer = ActorMaterializer()
+
+  // controlling backpressure
+  val controlledFlow = Flow[Int].map(_ * 2).buffer(10, OverflowStrategy.dropHead)
 
 }
