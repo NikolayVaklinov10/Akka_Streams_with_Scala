e9f09532602b3d2cd871372727515f531eb0c4e9
Nick <nikolayvaklinov@yahoo.com>
2020-09-09 16:33:44 +0100

    the standard way

--- src/main/scala/part4_techniques/AdvancedBackPressure.scala
+++ src/main/scala/part4_techniques/AdvancedBackPressure.scala
@@ -1,8 +1,10 @@
 package part4_techniques
 
+import java.util.Date
+
 import akka.actor.ActorSystem
 import akka.stream.{ActorMaterializer, OverflowStrategy}
-import akka.stream.scaladsl.Flow
+import akka.stream.scaladsl.{Flow, Sink, Source}
 
 object AdvancedBackPressure extends App {
 
@@ -11,5 +13,28 @@ object AdvancedBackPressure extends App {
 
   // controlling backpressure
   val controlledFlow = Flow[Int].map(_ * 2).buffer(10, OverflowStrategy.dropHead)
+
+  case class PagerEvent(description: String, date: Date)
+  case class Notification(email: String, pagerEvent: PagerEvent)
+
+  val events = List(
+    PagerEvent("Service discovery failed", new Date),
+    PagerEvent("Illegal elements in the data pipeline", new Date),
+    PagerEvent("Number of HTTP 500 spiked", new Date),
+    PagerEvent("A service stopped responding", new Date)
+  )
+  val eventSource = Source(events)
+
+  val oncallEngineer = "nick@rockthejvm.com" // a fast service for fetching oncall emails
+
+  def sendEmail(notification: Notification) =
+    println(s"Dear ${notification.email}, you have an event: ${notification.pagerEvent}") // actually send an email
+
+  val notificationSink = Flow[PagerEvent].map(event => Notification(oncallEngineer, event))
+    .to(Sink.foreach[Notification](sendEmail))
+
+  eventSource.to(notificationSink).run()
+
+
 
 }
