678f28d1e00cd32078947de364a673cc41c3ee69
Nick <nikolayvaklinov@yahoo.com>
2020-09-09 14:07:45 +0100

    common example of the external service

--- src/main/scala/part4_techniques/IntegratingWithExternalServices.scala
+++ src/main/scala/part4_techniques/IntegratingWithExternalServices.scala
@@ -3,10 +3,18 @@ package part4_techniques
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
 
+import scala.concurrent.Future
+
 object IntegratingWithExternalServices extends App {
 
   implicit val system = ActorSystem("IntegratingWithExternalServices")
   implicit val materializer = ActorMaterializer()
+
+
+  // external service may look like this
+  def genericExtService[A, B](element: A): Future[B] = ???
+
+
 
 
 
