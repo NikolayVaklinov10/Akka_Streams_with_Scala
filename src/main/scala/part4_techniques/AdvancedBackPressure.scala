c841258474024b2bbcae0794a5fca626ee8823f7
Nick <nikolayvaklinov@yahoo.com>
2020-09-09 16:15:06 +0100

    the usual implicits

--- src/main/scala/part4_techniques/AdvancedBackPressure.scala
+++ src/main/scala/part4_techniques/AdvancedBackPressure.scala
@@ -1,5 +1,11 @@
 package part4_techniques
 
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+
 object AdvancedBackPressure extends App {
+
+  implicit val system = ActorSystem("AdvancedBackPressure")
+  implicit val materializer = ActorMaterializer()
 
 }
