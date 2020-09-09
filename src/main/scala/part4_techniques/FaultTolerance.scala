b47dcf4cc430cb06728bbbd5b80958894d58a5bc
Nick <nikolayvaklinov@yahoo.com>
2020-09-09 17:27:50 +0100

    the usual implicits

--- src/main/scala/part4_techniques/FaultTolerance.scala
+++ src/main/scala/part4_techniques/FaultTolerance.scala
@@ -1,5 +1,11 @@
 package part4_techniques
 
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+
 object FaultTolerance extends App {
+
+  implicit val system = ActorSystem("FaultTolerance")
+  implicit val materializer = ActorMaterializer()
 
 }
