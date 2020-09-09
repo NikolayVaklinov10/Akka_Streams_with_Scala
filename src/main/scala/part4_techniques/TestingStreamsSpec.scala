779fda70390adfad447f4495aa8dff5d0edfe84f
Nick <nikolayvaklinov@yahoo.com>
2020-09-09 22:18:47 +0100

    first test of akka streams

--- src/main/scala/part4_techniques/TestingStreamsSpec.scala
+++ src/main/scala/part4_techniques/TestingStreamsSpec.scala
@@ -1,17 +1,36 @@
 package part4_techniques
 
 import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.{Keep, Sink, Source}
 import akka.testkit.TestKit
 import org.scalatest.{BeforeAndAfterAll, WordSpecLike}
+
+import scala.concurrent.Await
+import scala.concurrent.duration._
 
 class TestingStreamsSpec extends TestKit(ActorSystem("TestingStreamsSpec"))
   with WordSpecLike
   with BeforeAndAfterAll {
 
+  implicit val materializer = ActorMaterializer()
+
   override def afterAll(): Unit = {
     TestKit.shutdownActorSystem(system)
   }
 
-  
+  "A simple stream" should {
+    "satisfy basic assertions" in {
+      // description of the test
+      val simpleSource = Source(1 to 10)
+      val simpleSink = Sink.fold(0)((a: Int, b: Int) => a + b)
+
+      val sumFuture = simpleSource.toMat(simpleSink)(Keep.right).run()
+      val sum = Await.result(sumFuture, 2 seconds)
+      assert( sum == 55)
+    }
+  }
+
+
 
 }
