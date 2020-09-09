51459fe8029a97f1d920f3d2d6981a8bde7a093f
Nick <nikolayvaklinov@yahoo.com>
2020-09-09 22:32:08 +0100

    the second test

--- src/main/scala/part4_techniques/TestingStreamsSpec.scala
+++ src/main/scala/part4_techniques/TestingStreamsSpec.scala
@@ -3,7 +3,7 @@ package part4_techniques
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.{Keep, Sink, Source}
-import akka.testkit.TestKit
+import akka.testkit.{TestKit, TestProbe}
 import org.scalatest.{BeforeAndAfterAll, WordSpecLike}
 
 import scala.concurrent.Await
@@ -28,6 +28,18 @@ class TestingStreamsSpec extends TestKit(ActorSystem("TestingStreamsSpec"))
       val sumFuture = simpleSource.toMat(simpleSink)(Keep.right).run()
       val sum = Await.result(sumFuture, 2 seconds)
       assert( sum == 55)
+    }
+
+    "integrate with test actors via materialized values" in {
+      import akka.pattern.pipe
+      import system.dispatcher
+
+      val simpleSource = Source(1 to 10)
+      val simpleSink = Sink.fold(0)((a: Int, b: Int) => a + b)
+
+      val probe = TestProbe()
+
+      simpleSource.toMat(simpleSink)(Keep.right).run().pipeTo(probe.ref)
     }
   }
 
