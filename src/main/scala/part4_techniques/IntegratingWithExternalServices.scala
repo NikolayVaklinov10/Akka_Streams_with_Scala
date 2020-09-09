bc4d8ed7926d1742236a92273fad220d4c0d867e
Nick <nikolayvaklinov@yahoo.com>
2020-09-09 14:04:22 +0100

    the usual implicits for the akka streams

--- src/main/scala/part4_techniques/IntegratingWithExternalServices.scala
+++ src/main/scala/part4_techniques/IntegratingWithExternalServices.scala
@@ -1,7 +1,15 @@
 package part4_techniques
 
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+
 object IntegratingWithExternalServices extends App {
 
-  
+  implicit val system = ActorSystem("IntegratingWithExternalServices")
+  implicit val materializer = ActorMaterializer()
+
+
+
+
 
 }
