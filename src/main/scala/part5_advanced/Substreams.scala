487f2d3e8b422e66f3fa287cf2fee00a3077b3ea
Nick <nikolayvaklinov@yahoo.com>
2020-09-10 09:44:26 +0100

    the implicits

--- src/main/scala/part5_advanced/Substreams.scala
+++ src/main/scala/part5_advanced/Substreams.scala
@@ -1,5 +1,11 @@
 package part5_advanced
 
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+
 object Substreams extends App {
+
+  implicit val system = ActorSystem("Substreams")
+  implicit val materializer = ActorMaterializer()
 
 }
