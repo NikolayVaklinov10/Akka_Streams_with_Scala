a43c8a96cc0606fb64634a4677e41c231ca67160
Nick <nikolayvaklinov@yahoo.com>
2020-09-10 08:30:12 +0100

    the usual implicits

--- src/main/scala/part5_advanced/DynamicStreamHandling.scala
+++ src/main/scala/part5_advanced/DynamicStreamHandling.scala
@@ -1,6 +1,13 @@
 package part5_advanced
 
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+
 object DynamicStreamHandling extends App {
-  
+
+  implicit val system = ActorSystem("DynamicStreamHandling")
+  implicit val materializer = ActorMaterializer()
+
+
 
 }
