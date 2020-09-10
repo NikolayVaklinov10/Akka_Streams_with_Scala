f891415dc2e510a3ee925d4b3c9e540c93dfb6c6
Nick <nikolayvaklinov@yahoo.com>
2020-09-10 11:18:22 +0100

    merging the sub-streams back

--- src/main/scala/part5_advanced/Substreams.scala
+++ src/main/scala/part5_advanced/Substreams.scala
@@ -2,12 +2,16 @@ package part5_advanced
 
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
-import akka.stream.scaladsl.{Sink, Source}
+import akka.stream.scaladsl.{Keep, Sink, Source}
+
+import scala.util.{Failure, Success}
+
 
 object Substreams extends App {
 
   implicit val system = ActorSystem("Substreams")
   implicit val materializer = ActorMaterializer()
+  import system.dispatcher
 
   // 1 - grouping a stream by a certain function
   val wordSource = Source(List("Akka", "is", "amazing", "learning", "substreams"))
@@ -19,5 +23,27 @@ object Substreams extends App {
     newCount
   }))
     .run()
+
+  // 2 - merge substreams back
+  val textSource = Source(List(
+    "The price of Tesla shares are too high",
+    "at the moment the markets are overvalued",
+    "FED and the other central banks are responsible"
+  ))
+
+  val totalCharCountFuture = textSource
+    .groupBy(2, string => string.length % 2)
+    .map(_.length) // do the expensive computation here
+    .mergeSubstreamsWithParallelism(2)
+    .toMat(Sink.reduce[Int](_ + _))(Keep.right)
+    .run()
+
+  totalCharCountFuture.onComplete {
+    case Success(value) => println(s"Total char count: $value")
+    case Failure(ex) => println(s"Char computation failed: $ex")
+  }
+
+
+
 
 }
