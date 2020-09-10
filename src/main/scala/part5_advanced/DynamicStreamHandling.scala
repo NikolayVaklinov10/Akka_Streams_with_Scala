d021cbc734f13e762c3b882585c8476b91a2692b
Nick <nikolayvaklinov@yahoo.com>
2020-09-10 08:47:04 +0100

    the KillSwitches.single() is capable of shutting down a a stream at a point where I need it

--- src/main/scala/part5_advanced/DynamicStreamHandling.scala
+++ src/main/scala/part5_advanced/DynamicStreamHandling.scala
@@ -1,12 +1,32 @@
 package part5_advanced
 
 import akka.actor.ActorSystem
-import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.{Keep, Sink, Source}
+import akka.stream.{ActorMaterializer, KillSwitches}
+
+import scala.concurrent.duration._
 
 object DynamicStreamHandling extends App {
 
   implicit val system = ActorSystem("DynamicStreamHandling")
   implicit val materializer = ActorMaterializer()
+  import system.dispatcher
+
+  // #1: Kill Switch
+  val killSwitchFlow = KillSwitches.single[Int]
+  val counter = Source(Stream.from(1)).throttle(1, 1 second).log("counter")
+  val sink = Sink.ignore
+
+  val killSwitch = counter
+    .viaMat(killSwitchFlow)(Keep.right)
+    .to(sink)
+    .run()
+
+  system.scheduler.scheduleOnce(3 seconds){
+    killSwitch.shutdown()
+  }
+
+
 
 
 
