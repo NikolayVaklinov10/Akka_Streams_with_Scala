51af3c980cbcc7c4c731003f9f4cda843ece391a
Nick <nikolayvaklinov@yahoo.com>
2020-09-13 10:18:45 +0100

    the standard implicits values

--- src/main/scala/part5_advanced/CustomOperators.scala
+++ src/main/scala/part5_advanced/CustomOperators.scala
@@ -1,6 +1,11 @@
 package part5_advanced
 
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+
 object CustomOperators extends App {
-  
+  implicit val system = ActorSystem("CustomOperators")
+  implicit val materializer = ActorMaterializer()
+
 
 }
