6b0585c47cdd61f7074dfa197284e9b48dfb60a2
Nick <nikolayvaklinov@yahoo.com>
2020-09-14 18:07:01 +0100

    the random number generator class

--- src/main/scala/part5_advanced/CustomOperators.scala
+++ src/main/scala/part5_advanced/CustomOperators.scala
@@ -1,11 +1,47 @@
 package part5_advanced
 
 import akka.actor.ActorSystem
-import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.{Sink, Source}
+import akka.stream.{ActorMaterializer, Attributes, Outlet, SourceShape}
+import akka.stream.stage.{GraphStage, GraphStageLogic, OutHandler}
+
+import scala.util.Random
 
 object CustomOperators extends App {
   implicit val system = ActorSystem("CustomOperators")
   implicit val materializer = ActorMaterializer()
+
+  // 1 - a custom source which emit random numbers until canceled
+
+  class RandomNumberGenerator(max: Int) extends GraphStage[/*step 0: define the shape*/SourceShape[Int]] {
+
+    // step 1: define the ports and the component-specific members
+    val outPort = Outlet[Int]("randomGenerator")
+    val random = new Random()
+
+    // step 2: construct a new shape
+    override def shape: SourceShape[Int] = SourceShape(outPort)
+
+    // step 3: create state
+    override def createLogic(inheritedAttributes: Attributes): GraphStageLogic = new GraphStageLogic(shape) {
+      // step 4:
+      // define mutable state
+      // implement my logic here
+
+      setHandler(outPort, new OutHandler {
+        // when there is demand from downstream
+        override def onPull(): Unit = {
+          // emit a new element
+          val nextNumber = random.nextInt(max)
+          // push it out of the outPort
+          push(outPort, nextNumber)
+        }
+      })
+    }
+  }
+
+  val randomGeneratorSource = Source.fromGraph(new RandomNumberGenerator(100))
+//  randomGeneratorSource.runWith(Sink.foreach(println))
 
 
 }
