0279674c4f4cdefbc22ba642f3b92b5e5312b8fb
Nick <nikolayvaklinov@yahoo.com>
2020-09-15 22:51:45 +0100

    the customer sink part of the code

--- src/main/scala/part5_advanced/CustomOperators.scala
+++ src/main/scala/part5_advanced/CustomOperators.scala
@@ -2,9 +2,10 @@ package part5_advanced
 
 import akka.actor.ActorSystem
 import akka.stream.scaladsl.{Sink, Source}
-import akka.stream.{ActorMaterializer, Attributes, Outlet, SourceShape}
-import akka.stream.stage.{GraphStage, GraphStageLogic, OutHandler}
+import akka.stream.{ActorMaterializer, Attributes, Inlet, Outlet, SinkShape, SourceShape}
+import akka.stream.stage.{GraphStage, GraphStageLogic, InHandler, OutHandler}
 
+import scala.collection.mutable
 import scala.util.Random
 
 object CustomOperators extends App {
@@ -42,6 +43,48 @@ object CustomOperators extends App {
 
   val randomGeneratorSource = Source.fromGraph(new RandomNumberGenerator(100))
 //  randomGeneratorSource.runWith(Sink.foreach(println))
+
+  // 2 - a custom sink that prints elements in a batches of a given size
+
+  class Batcher(batchSize: Int) extends GraphStage[SinkShape[Int]] {
+
+    val inPort = Inlet[Int]("batcher")
+
+    override def shape: SinkShape[Int] = SinkShape[Int](inPort)
+
+    override def createLogic(inheritedAttributes: Attributes): GraphStageLogic = new GraphStageLogic(shape) {
+
+      override def preStart(): Unit = {
+        pull(inPort)
+      }
+
+      // mutable state
+      val batch = new mutable.Queue[Int]
+
+      setHandler(inPort, new InHandler {
+        // when the upstream wants to send me an element
+        override def onPush(): Unit = {
+          val nextElement = grab(inPort)
+          batch.enqueue(nextElement)
+
+          // assume some complex computation
+          Thread.sleep(100)
+
+          if(batch.size >= batchSize) {
+            println("New batch: " + batch.dequeueAll(_ => true).mkString("[", ", ", "]"))
+          }
+          pull(inPort) // send demand upstream
+        }
+
+        override def onUpstreamFinish(): Unit = {
+          if (batch.nonEmpty) {
+            println("New batch: " + batch.dequeueAll(_ => true).mkString("[", ", ", "]"))
+            println("Stream finished.")
+          }
+        }
+      })
+    }
+  }
 
 
 }
